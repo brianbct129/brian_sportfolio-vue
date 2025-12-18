f13f93878c8756bd660b3d978bbae1c9b452e0bd
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2025-12-18 17:53:03 +0700

    feat(TechStack): refactor tech stack display to use dynamic data
    - Replace hardcoded tech items with dynamic techList array
    - Add support for dark mode images where available
    - Include new tech stack items (Visual Paradigm, Trello, Vue.js, etc)
    - Maintain progress display functionality while making it data-driven

--- src/components/TechStack.vue
+++ src/components/TechStack.vue
@@ -9,78 +9,47 @@
       tools drives better results
     </h4>
     <ul class="tech-list">
-      <li class="wg-tech">
-        <div class="tech-infor effectFade fadeUp no-div">
-          <div class="tech_image">
-            <img loading="lazy" width="19" height="28" src="../assets/images/section/tech-1.svg" alt="Image">
+      <template v-for="(tech, index) in techList" :key="tech.name">
+        <li class="wg-tech">
+          <div class="tech-infor effectFade fadeUp no-div">
+            <div class="tech_image">
+              <img
+                loading="lazy"
+                :width="tech.w"
+                :height="tech.h"
+                :src="isDark && tech.srcDark ? tech.srcDark : tech.src"
+                :alt="tech.name"
+              >
+            </div>
+            <div class="tech_info">
+              <p class="info__name fw-medium text-black-72">{{ tech.name }}</p>
+              <p class="info__duty text-black-56 text-body-3">{{ tech.desc }}</p>
+            </div>
           </div>
-          <div class="tech_info">
-            <p class="info__name fw-medium text-black-72">Figma</p>
-            <p class="info__duty text-black-56 text-body-3">Leading design tool</p>
+          <div class="tech-progress">
+            <div class="progress-line" :data-progress="tech.progress">
+              <p class="progress-num text-caption">
+                <span class="counter">
+                  <span class="number" data-speed="1500" :data-to="tech.progress">{{ tech.progress }}</span>
+                  %
+                </span>
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="tech-progress">
-          <div class="progress-line" data-progress="80">
-            <p class="progress-num text-caption">
-              <span class="counter">
-                <span class="number" data-speed="1500" data-to="80">80</span>
-                %
-              </span>
-            </p>
-          </div>
-        </div>
-      </li>
-      <li class="br-line"></li>
-      <li class="wg-tech">
-        <div class="tech-infor effectFade fadeUp no-div">
-          <div class="tech_image">
-            <img :src="isDark ? tech2Dark : tech2Light" width="18" height="28" alt="Image">
-          </div>
-          <div class="tech_info">
-            <p class="info__name fw-medium text-black-72">Framer</p>
-            <p class="info__duty text-black-56 text-body-3">No-code website builder</p>
-          </div>
-        </div>
-        <div class="tech-progress">
-          <div class="progress-line" data-progress="90">
-            <p class="progress-num text-caption">
-              <span class="counter">
-                <span class="number" data-speed="1500" data-to="90">90</span>
-                %
-              </span>
-            </p>
-          </div>
-        </div>
-      </li>
-      <li class="br-line"></li>
-      <li class="wg-tech">
-        <div class="tech-infor effectFade fadeUp no-div">
-          <div class="tech_image">
-            <img loading="lazy" width="29" height="28" src="../assets/images/section/tech-3.svg" alt="Image">
-          </div>
-          <div class="tech_info">
-            <p class="info__name fw-medium text-black-72">Adobe Photoshop</p>
-            <p class="info__duty text-black-56 text-body-3">Raster graphics editor</p>
-          </div>
-        </div>
-        <div class="tech-progress">
-          <div class="progress-line" data-progress="60">
-            <p class="progress-num text-caption">
-              <span class="counter">
-                <span class="number" data-speed="1500" data-to="60">10</span>
-                %
-              </span>
-            </p>
-          </div>
-        </div>
-      </li>
+        </li>
+        <li v-if="index < techList.length - 1" class="br-line"></li>
+      </template>
     </ul>
   </div>
 </template>
 <script setup>
-import { ref, onMounted, onBeforeUnmount, onUpdated, nextTick } from 'vue'
+  import { ref, onMounted, onBeforeUnmount, onUpdated, nextTick } from 'vue'
 import tech2Light from '../assets/images/section/tech-2.svg'
 import tech2Dark from '../assets/images/section/tech-2_dark.svg'
+import tech3 from '../assets/images/section/tech-3.svg'
+import light_sql from '../assets/images/section/light_sql.png'
+import visual_paradigm from '../assets/images/section/visual_paradigm.png'
+import trello from '../assets/images/section/trello.png'
 
 const isDark = ref(false)
 let observer = null
@@ -106,4 +75,87 @@ onUpdated(() => {
 onBeforeUnmount(() => {
   if (observer) observer.disconnect()
 })
+const techList = [
+   {
+    name: 'Visual Paradigm',
+    desc: 'Visual modeling & design',
+    src: visual_paradigm,
+    w: 30,
+    h: 28,
+    progress: 100,
+  },
+  {
+    name: 'Trello',
+    desc: 'Project management & collaboration',
+    src: trello,
+    w: 30,
+    h: 28,
+    progress: 90,
+  },
+  {
+    name: 'Vue.js',
+    desc: 'SPA & front-end architecture',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png',
+    w: 30,
+    h: 28,
+    progress: 85,
+  },
+  {
+    name: 'Flask',
+    desc: 'API & backend development',
+    src: 'https://hackersandslackers-cdn.storage.googleapis.com/2024/09/Flask_Series.png',
+    w: 38,
+    h: 28,
+    progress: 75,
+  },
+   {
+    name: 'Django',
+    desc: 'API & backend development',
+    src: 'https://www.svgrepo.com/show/353657/django-icon.svg',
+    w: 38,
+    h: 28,
+    progress: 70,
+  },
+  {
+    name: 'MySQL',
+    desc: 'Database management',
+    src: 'https://upload.wikimedia.org/wikipedia/labs/8/8e/Mysql_logo.png',
+    srcDark: light_sql,
+    w: 40,
+    h: 28,
+    progress: 80,
+  },
+  {
+    name: 'Adobe Photoshop',
+    desc: 'UI assets & visual editing',
+    src: tech3,
+    w: 29,
+    h: 28,
+    progress: 80,
+  },
+  {
+    name: 'Laravel',
+    desc: 'API & backend development',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1969px-Laravel.svg.png',
+    w: 29,
+    h: 28,
+    progress: 88,
+  },
+  {
+    name: 'Tailwind',
+    desc: 'Responsive UI components',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1024px-Tailwind_CSS_Logo.svg.png?20230715030042',
+    w: 35,
+    h: 28,
+    progress: 67,
+  },
+  {
+    name: 'Bootstrap',
+    desc: 'Responsive UI components',
+    src: 'https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow@2x.png',
+    w: 35,
+    h: 28,
+    progress: 100,
+  },
+]
 </script>
